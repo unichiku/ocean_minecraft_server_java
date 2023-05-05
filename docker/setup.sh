be626aa4cc0003deac3139ce1342eec0dc73a4c4
unichiku <unichiku@unichikus-Mac-mini.local>
2023-05-05 21:56:33 +0900

    [feat] manual edit eula & server.properties

--- docker/setup.sh
+++ docker/setup.sh
@@ -2,5 +2,3 @@ cp -r /root/minecraft /mnt/minecraft
 cd /mnt/minecraft
 
 java -Xmx2048M -Xms2048M -jar spigot-${VERSION}.jar nogui
-cat eula.txt | sed "s/eula=false/eula=true/g" >> eula.txt
-java -Xmx2048M -Xms2048M -jar spigot-${VERSION}.jar nogui
